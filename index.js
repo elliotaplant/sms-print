739bc74e595415e82fd4d3dc5b22c3bc91f17f11
Elliot Plant <elliotaplant@gmail.com>
2017-12-22 16:17:56 -0800

    Get example test message sent

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const twilio = require('twilio')
-const { accountSid, authToken } = require('twilio-keys.json');
+const { accountSid, authToken } = require('./twilio-keys.json');
 
 const client = new twilio(accountSid, authToken);
 
 client.messages.create({
     body: 'Hello from Node',
-    to: '+5109175552',  // Personal number
-    from: '14155793449' // My twilio number
+    to: '+15109175552',  // Personal number
+    from: '+14155793449' // My twilio number
 })
 .then((message) => console.log(message.sid))
 .catch(error => console.error('No good', error));

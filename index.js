bc9e99f7b0a4ac195ee8b964b30d898ecf32f591
Elliot Plant <elliotaplant@gmail.com>
2017-12-22 16:16:51 -0800

    cp

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var Hapi = require('hapi');
+const twilio = require('twilio')
+const { accountSid, authToken } = require('twilio-keys.json');
 
-var server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 8000
-});
+const client = new twilio(accountSid, authToken);
 
-server.start();
+client.messages.create({
+    body: 'Hello from Node',
+    to: '+5109175552',  // Personal number
+    from: '14155793449' // My twilio number
+})
+.then((message) => console.log(message.sid))
+.catch(error => console.error('No good', error));
